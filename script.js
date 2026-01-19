f5f013589df95f4ebbedc64768a3b622e24268ce
novalnns <novalna73@gmail.com>
2026-01-19 16:26:08 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+let slides = document.querySelectorAll('.slide');
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 3000); // ganti slide tiap 3 detik
+</script>
